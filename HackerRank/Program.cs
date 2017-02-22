0ff9109fc8af3d53c4ef1c723855ad647f49ad4c
michigandentist@netscape.net <rxh5MmBt2Q>
2017-02-22 17:38:55 -0500

    This times out on tests 13,14 & 15.

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -13,37 +13,30 @@ namespace HackerRank
 			Int32 size = Int32.Parse (h_temp [0]);
 			Int32 swaps = Int32.Parse (h_temp [1]);
 			h_temp = Console.ReadLine ().Split (' '); // get array values
-			var nums = Array.ConvertAll (h_temp, int.Parse); // assign as int array
-			var currentIndex = 0;
+			var nums = Array.ConvertAll (h_temp, Int32.Parse).ToList(); // assign as int array															//var currentIndex = 0;
+			var index = 0;
+			var temp = 0;
 
-			for (int i = 0; i < swaps; i++) 
+			for (int i = 0; i < size; i++) 
 			{
-				if (currentIndex < size) 
+				if (swaps > 0) 
 				{
-					var high = 0;
-					for (int x = currentIndex; x < size; x++) 
+					// skip sorted members and get index of remaining max value.
+					index = nums.IndexOf (nums.Skip (i).Max ());
+					if (nums [index] > nums [i]) 
 					{
-						if (nums [high] < nums [x]) // fix index references
-						{
-							high = x;
-						}
-					}
-					if (nums [high] > nums [currentIndex] && high > 0) 
-					{
-						var temp = nums [currentIndex];
-						nums [currentIndex] = nums [high];
-						nums [high] = temp;
-						currentIndex++;
+						
+						temp = nums [i];
+						nums [i] = nums [index];
+						nums [index] = temp;
+						swaps--;
 					}
 				}
 			}
-			string output = null;
 			for (int n = 0; n < size;n++)
 			{
-				output += (nums [n] + " ");
+				Console.Write (nums [n] + " ");
 			}
-			Console.WriteLine ();
-			Console.WriteLine (output);
 		}
 	}
 }
