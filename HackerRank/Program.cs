9f1bc026076f392bf7159718d3db151b1227a856
michigandentist@netscape.net <rxh5MmBt2Q>
2017-02-22 00:07:20 -0500

    The indexes are messed up. Figure out how to progress through and keep track of largest remaining int and where it goes to.

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -5,47 +5,45 @@ using System.Collections.Generic;
 
 namespace HackerRank
 {
-<<<<<<< HEAD
-	class MainClass//Grid Challenge
-=======
-	class MainClass //Designer PDF Viewer
->>>>>>> cuttingBoards
+	class MainClass //Largest Permutation
 	{
 		public static void Main (string [] args)
 		{
-			string [] h_temp = Console.ReadLine ().Split (' ');// Load height values of the alphabet
-			int [] h = Array.ConvertAll (h_temp, Int32.Parse);// convert them to int
-			string word = Console.ReadLine ();// get the word to enclose in a box
-			char [] letters = word.ToCharArray ();// convert the string to a char array
+			string [] h_temp = Console.ReadLine ().Split (' ');// Get Array size and number of Swaps allowed
+			Int32 size = Int32.Parse (h_temp [0]);
+			Int32 swaps = Int32.Parse (h_temp [1]);
+			h_temp = Console.ReadLine ().Split (' '); // get array values
+			var nums = Array.ConvertAll (h_temp, int.Parse); // assign as int array
+			var currentIndex = 0;
 
-			// Find the tallest letter
-			int maxHeight = 0;
-<<<<<<< HEAD
-			for (int x = 0; x < letters.Length; x++) {
-				int thisHeight = h [char.ToUpper (letters [x]) - 64 - 1];
-				// int anotherWay = h[((int)letters [x] % 32) - 1];
-				if (maxHeight < thisHeight) {
-=======
-			for (int x = 0; x < letters.Length; x++)
+			for (int i = 0; i < swaps; i++) 
 			{
-				int thisHeight = h[char.ToUpper (letters [x])-64 - 1];
-				int anotherWay = h[((int)letters [x] % 32) - 1];
-				if (maxHeight< thisHeight)
+				if (currentIndex < size) 
 				{
->>>>>>> cuttingBoards
-					maxHeight = thisHeight;
+					var high = 0;
+					for (int x = currentIndex; x < size; x++) 
+					{
+						if (nums [high] < nums [x]) // fix index references
+						{
+							high = x;
+						}
+					}
+					if (nums [high] > nums [currentIndex] && high > 0) 
+					{
+						var temp = nums [currentIndex];
+						nums [currentIndex] = nums [high];
+						nums [high] = temp;
+						currentIndex++;
+					}
 				}
 			}
-
-<<<<<<< HEAD
-			// Multiply maxHeight times length of the word
-			int areaToHilight = word.Length * maxHeight;
-			Console.WriteLine (areaToHilight);
-=======
-			// Multiply hieght times length of the word
-			int areaToHilight = word.Length * maxHeight;
-			Console.WriteLine (areaToHilight);
+			string output = null;
+			for (int n = 0; n < size;n++)
+			{
+				output += (nums [n] + " ");
+			}
+			Console.WriteLine ();
+			Console.WriteLine (output);
 		}
->>>>>>> cuttingBoards
 	}
 }
