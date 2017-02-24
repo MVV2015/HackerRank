3fddd3da078eaa87349fa5ad9177570467da1311
michigandentist@netscape.net <rxh5MmBt2Q>
2017-02-23 21:34:20 -0500

    Largest Permutation Dictionary look-up

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -10,27 +10,44 @@ namespace HackerRank
 		public static void Main (string [] args)
 		{
 			string [] h_temp = Console.ReadLine ().Split (' ');// Get Array size and number of Swaps allowed
-			Int32 size = Int32.Parse (h_temp [0]);
-			Int32 swaps = Int32.Parse (h_temp [1]);
-			h_temp = Console.ReadLine ().Split (' '); // get array values
-			var nums = Array.ConvertAll (h_temp, Int32.Parse).ToList(); // assign as int array															//var currentIndex = 0;
-			var index = 0;
-			var temp = 0;
+			int size = Int32.Parse (h_temp [0]);
+			int swaps = Int32.Parse (h_temp [1]);
+			h_temp = Console.ReadLine ().Split (' '); // get array values. (all Natural numbers from 1 to N)
+			int[] nums = Array.ConvertAll (h_temp, Int32.Parse); // assign as int array
+			Dictionary <int, int > numPair = new Dictionary<int, int > ();
 
-			for (int i = 0; i < size; i++) 
+			// Assign array values as dictionary keys. The value for each key is assigned a default int.
+			// The ints start at 0 and incerment up from there. Just happens to correspond to the array indexes.
+			for (int n = 0; n < nums.Length; n++) 
 			{
-				if (swaps > 0) 
+				numPair [nums [n]] = n;
+			}
+
+			int temp = 0;
+			int largestNum = nums.Length; // The largest num in the array corresponds to the array size.
+			int largestNumIndex; // Used to store the location of the largest value in the array.
+
+			for (int i = 0; i < size; i++) // Step through the entire array.
+			{
+				if (swaps > 0) // Limit the number of swaps (ref input (swaps)
 				{
-					// skip sorted members and get index of remaining max value.
-					index = nums.IndexOf (nums.Skip (i).Max ());
-					if (nums [index] > nums [i]) 
-					{
-						
+					// Get the index of the largest remaining number from the Dictionary.
+					largestNumIndex = numPair [largestNum]; 
+
+					// Make sure it is Larger than the next int in the array.
+					// Note the use of the indexer (i) to keep track of the placement of the value being compared to.
+					if (nums [largestNumIndex] > nums [i])// Swap the values if needed 
+					{						
 						temp = nums [i];
-						nums [i] = nums [index];
-						nums [index] = temp;
+						nums [i] = nums [largestNumIndex];
+						nums [largestNumIndex] = temp;
 						swaps--;
 					}
+					largestNum--;
+
+					// Trade the dictionary items
+					numPair [nums [largestNumIndex]] = largestNumIndex;
+					numPair [nums [i]] = i;
 				}
 			}
 			for (int n = 0; n < size;n++)
