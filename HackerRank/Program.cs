18c8b1a41639bdd758e8509fb2d97ec8f7857125
michigandentist@netscape.net <rxh5MmBt2Q>
2017-02-26 18:55:09 -0500

    Kangaroo hops

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -1,56 +1,28 @@
 ﻿using System;
-using System.Linq;
-using System.Collections.Generic;
 
 
 namespace HackerRank
 {
-	class MainClass //Apple and Orange
+	class MainClass //Kangaroo
 	{
 		public static void Main (string [] args)
 		{
-			string [] h_temp = Console.ReadLine ().Split (' ');// Get Start and end point of house
-			int houseStart = Int32.Parse (h_temp [0]);
-			int houseEnd = Int32.Parse (h_temp [1]);
+			string [] tokens_x1 = Console.ReadLine ().Split (' ');
+			int x1 = Convert.ToInt32 (tokens_x1 [0]);
+			int v1 = Convert.ToInt32 (tokens_x1 [1]);
+			int x2 = Convert.ToInt32 (tokens_x1 [2]);
+			int v2 = Convert.ToInt32 (tokens_x1 [3]);
 
-			h_temp = Console.ReadLine ().Split (' '); // get location of trees
-			int appleTree = Int32.Parse (h_temp [0]);
-			int orangeTree = Int32.Parse (h_temp [1]);
-
-			h_temp = Console.ReadLine ().Split (' '); // numbers of apples and oranges
-			int apples = Int32.Parse (h_temp [0]);
-			int oranges = Int32.Parse (h_temp [1]);
-
-			h_temp = Console.ReadLine ().Split (' ');
-			int[] appleDist = Array.ConvertAll (h_temp, Int32.Parse); // Distances apples fall from tree
-
-			h_temp = Console.ReadLine ().Split (' ');
-			int [] orangeDist = Array.ConvertAll (h_temp, Int32.Parse); // Diatances oranges fall from tree
-
-			int appleHits = 0;
-			int orangeHits = 0;
-			int dropLocation = 0;
-
-			for (int a = 0; a < apples; a++)
-			{
-				dropLocation = appleDist [a] + appleTree;
-				if( dropLocation >= houseStart && dropLocation <= houseEnd)
-				{
-					appleHits++;
-				}
+			if (((x1 >= x2 && v1 <= v2) || (x1 <= x2 && v1 >= v2))
+				&& !(x1 != x2 && v1 == v2)
+				&& (Math.Abs (x1 - x2) % Math.Abs (v1 - v2) == 0)) {
+				Console.WriteLine ("YES");
+			} else {
+				Console.WriteLine ("NO");
 			}
 
-			for (int o = 0; o < oranges; o++)
-			{
-				dropLocation = orangeDist [o] + orangeTree;
-				if (dropLocation >= houseStart && dropLocation <= houseEnd) 
-				{
-					orangeHits++;
-				}
-			}
 
-			Console.WriteLine (appleHits);
-			Console.WriteLine (orangeHits);
+
 		}
 	}
 }
